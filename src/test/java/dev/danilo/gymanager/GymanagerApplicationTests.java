b3c2c4169fe3d8e4618f1295f8053736e832680c
danilodinizs <danilodiniz93@yahoo.com>
2025-04-01 11:08:15 -0300

    Initial commit

---
+++ src/test/java/dev/danilo/gymanager/GymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danilo.gymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
