3988720b3f5a15236c7737f824b0dc6fa4d76d92
danilodinizs <danilodiniz93@yahoo.com>
2025-04-02 17:36:06 -0300

    feat: implements the getAll endpoint bussiness rule

--- src/main/java/dev/danilo/gymanager/service/SpreadsheetService.java
+++ src/main/java/dev/danilo/gymanager/service/SpreadsheetService.java
@@ -1,7 +1,26 @@
 package dev.danilo.gymanager.service;
 
+import dev.danilo.gymanager.dto.SpreadsheetResponseDTO;
+import dev.danilo.gymanager.entity.Spreadsheet;
+import dev.danilo.gymanager.mapper.SpreadsheetMapper;
+import dev.danilo.gymanager.repository.SpreadsheetRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SpreadsheetService {
+
+    private final SpreadsheetRepository repository;
+
+    private final SpreadsheetMapper mapper;
+
+    public SpreadsheetService(SpreadsheetRepository repository, SpreadsheetMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public List<SpreadsheetResponseDTO> findAll() {
+        return mapper.toDtoList(repository.findAll());
+    }
 }
