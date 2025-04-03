98524d643e2a8039bb38b1dcc0b6839d536c23f4
danilodinizs <danilodiniz93@yahoo.com>
2025-04-03 16:49:36 -0300

    feat: implements Workout Entity

--- src/main/java/dev/danilo/gymanager/entity/Workout.java
+++ src/main/java/dev/danilo/gymanager/entity/Workout.java
@@ -1,16 +1,37 @@
 package dev.danilo.gymanager.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.DayOfWeek;
+import java.util.List;
 import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
+@Table(name = "workout")
 public class Workout {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    private String description;
+    private DayOfWeek dayOfWeek;
+
+    @ManyToOne
+    @JoinColumn(name = "spreadsheet_id")
+    private Spreadsheet spreadsheet;
+
+    @OneToMany//(mappedBy = "workout")
+    private List<Exercise> exercises;
+
+
 }
