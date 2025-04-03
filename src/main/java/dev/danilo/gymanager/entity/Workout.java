fe46ec84dfbdfbde9aedcfad7004833307a390f4
danilodinizs <danilodiniz93@yahoo.com>
2025-04-03 11:02:27 -0300

    fix: adds ID annotation

--- src/main/java/dev/danilo/gymanager/entity/Workout.java
+++ src/main/java/dev/danilo/gymanager/entity/Workout.java
@@ -1,7 +1,16 @@
 package dev.danilo.gymanager.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
 
 @Entity
 public class Workout {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
 }
