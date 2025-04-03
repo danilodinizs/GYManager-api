aa492180c2e9febabb111a7d32a5cb95eee4427a
danilodinizs <danilodiniz93@yahoo.com>
2025-04-03 16:48:12 -0300

    fix: adds builder annotation to fix MapperDTO

--- src/main/java/dev/danilo/gymanager/dto/SpreadsheetRequestDTO.java
+++ src/main/java/dev/danilo/gymanager/dto/SpreadsheetRequestDTO.java
@@ -1,14 +1,21 @@
 package dev.danilo.gymanager.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import dev.danilo.gymanager.entity.Workout;
+import lombok.Builder;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+@Builder
 public record SpreadsheetRequestDTO(UUID id,
                                     String name,
                                     String description,
-                                    LocalDateTime date,
+                                    @JsonFormat(pattern = "dd/MM/yyyy")
+                                    LocalDate date,
                                     List<Workout> workouts) {
+
+    public SpreadsheetRequestDTO {
+    }
 }
