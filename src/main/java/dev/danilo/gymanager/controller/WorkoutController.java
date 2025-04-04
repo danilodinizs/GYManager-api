7b74dcb9609a74f9350722bc8b05ba1109d6148b
danilodinizs <danilodiniz93@yahoo.com>
2025-04-04 17:41:36 -0300

    feat: adds save workout endpoint

--- src/main/java/dev/danilo/gymanager/controller/WorkoutController.java
+++ src/main/java/dev/danilo/gymanager/controller/WorkoutController.java
@@ -1,7 +1,13 @@
 package dev.danilo.gymanager.controller;
 
+import dev.danilo.gymanager.dto.WorkoutRequestDTO;
+import dev.danilo.gymanager.dto.WorkoutResponseDTO;
 import dev.danilo.gymanager.mapper.WorkoutMapper;
 import dev.danilo.gymanager.service.WorkoutService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +23,10 @@ public class WorkoutController {
     public WorkoutController(WorkoutService service, WorkoutMapper mapper) {
         this.service = service;
         this.mapper = mapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<WorkoutResponseDTO> saveWorkout(@RequestBody WorkoutRequestDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.saveWorkout(dto));
     }
 }
