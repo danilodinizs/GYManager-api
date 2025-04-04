e98046020284cc97be6c15d3385743c493584595
danilodinizs <danilodiniz93@yahoo.com>
2025-04-04 10:03:50 -0300

    feat: injects the necessary dependencies

--- src/main/java/dev/danilo/gymanager/controller/WorkoutController.java
+++ src/main/java/dev/danilo/gymanager/controller/WorkoutController.java
@@ -1,4 +1,21 @@
 package dev.danilo.gymanager.controller;
 
+import dev.danilo.gymanager.mapper.WorkoutMapper;
+import dev.danilo.gymanager.service.WorkoutService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/workout")
 public class WorkoutController {
+
+    private final WorkoutService service;
+
+    private final WorkoutMapper mapper;
+
+
+    public WorkoutController(WorkoutService service, WorkoutMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
 }
--- src/main/java/dev/danilo/gymanager/service/WorkoutService.java
+++ src/main/java/dev/danilo/gymanager/service/WorkoutService.java
@@ -1,4 +1,17 @@
 package dev.danilo.gymanager.service;
 
+import dev.danilo.gymanager.mapper.WorkoutMapper;
+import dev.danilo.gymanager.repository.WorkoutRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class WorkoutService {
+
+    private final WorkoutRepository repository;
+    private final WorkoutMapper mapper;
+
+    public WorkoutService(WorkoutRepository repository, WorkoutMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
 }
