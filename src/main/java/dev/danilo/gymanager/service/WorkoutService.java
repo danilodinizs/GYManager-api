48a3d8f775a91f48be9c7e284dc6bb0585585686
danilodinizs <danilodiniz93@yahoo.com>
2025-04-04 17:42:03 -0300

    feat: adds save workout endpoint business rule

--- src/main/java/dev/danilo/gymanager/service/WorkoutService.java
+++ src/main/java/dev/danilo/gymanager/service/WorkoutService.java
@@ -1,7 +1,13 @@
 package dev.danilo.gymanager.service;
 
+import dev.danilo.gymanager.dto.WorkoutRequestDTO;
+import dev.danilo.gymanager.dto.WorkoutResponseDTO;
+import dev.danilo.gymanager.entity.Spreadsheet;
+import dev.danilo.gymanager.entity.Workout;
 import dev.danilo.gymanager.mapper.WorkoutMapper;
+import dev.danilo.gymanager.repository.SpreadsheetRepository;
 import dev.danilo.gymanager.repository.WorkoutRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +16,31 @@ public class WorkoutService {
     private final WorkoutRepository repository;
     private final WorkoutMapper mapper;
 
-    public WorkoutService(WorkoutRepository repository, WorkoutMapper mapper) {
+    public WorkoutService(WorkoutRepository repository, WorkoutMapper mapper, SpreadsheetRepository spreadsheetRepository) {
         this.repository = repository;
         this.mapper = mapper;
+        this.spreadsheetRepository = spreadsheetRepository;
+    }
+
+    private final SpreadsheetRepository spreadsheetRepository;
+
+
+
+    @Transactional
+    public WorkoutResponseDTO saveWorkout(WorkoutRequestDTO dto) {
+        Spreadsheet spreadsheet = spreadsheetRepository.findById(dto.spreadsheetId())
+                .orElseThrow(() -> new RuntimeException("Spreadsheet não encontrado"));
+
+        // 2. Cria o novo Workout e vincula ao Spreadsheet
+        Workout workout = new Workout();
+        workout.setName(dto.name());
+        workout.setDescription(dto.description());
+        workout.setDayOfWeek(dto.dayOfWeek());
+        workout.setSpreadsheet(spreadsheet); // Vinculação manual
+
+        // 3. Adiciona o Workout à lista do Spreadsheet (bidirecional)
+        spreadsheet.getWorkouts().add(workout); // Isso é essencial!
+
+        return mapper.toDto(repository.save(workout));
     }
 }
