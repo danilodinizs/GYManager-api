fb67f68cd01f621d4709e47724b4c9a5bcd79056
danilodinizs <danilodiniz93@yahoo.com>
2025-04-04 17:40:54 -0300

    feat: includes Getters and Setters

--- src/main/java/dev/danilo/gymanager/entity/Spreadsheet.java
+++ src/main/java/dev/danilo/gymanager/entity/Spreadsheet.java
@@ -1,10 +1,10 @@
 package dev.danilo.gymanager.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -29,6 +29,47 @@ public class Spreadsheet {
     @Column(name = "created_at")
     private LocalDate date;
 
-    @OneToMany(mappedBy = "spreadsheet")
+    @OneToMany(mappedBy = "spreadsheet", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
     private List<Workout> workouts = new ArrayList<>();
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public List<Workout> getWorkouts() {
+        return workouts;
+    }
+
+    public void setWorkouts(List<Workout> workouts) {
+        this.workouts = workouts;
+    }
 }
--- src/main/java/dev/danilo/gymanager/entity/Workout.java
+++ src/main/java/dev/danilo/gymanager/entity/Workout.java
@@ -1,5 +1,6 @@
 package dev.danilo.gymanager.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -29,12 +30,60 @@ public class Workout {
     @Enumerated(EnumType.STRING)
     private DayOfWeek dayOfWeek;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "spreadsheet_id")
     private Spreadsheet spreadsheet;
 
-    @OneToMany//(mappedBy = "workout")
+    @OneToMany(mappedBy = "workout")
+    @JsonIgnore
     private List<Exercise> exercises = new ArrayList<>();
 
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Spreadsheet getSpreadsheet() {
+        return spreadsheet;
+    }
+
+    public void setSpreadsheet(Spreadsheet spreadsheet) {
+        this.spreadsheet = spreadsheet;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(DayOfWeek dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public List<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(List<Exercise> exercises) {
+        this.exercises = exercises;
+    }
 }
