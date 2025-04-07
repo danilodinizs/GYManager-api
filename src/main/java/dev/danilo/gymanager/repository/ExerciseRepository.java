df2a4029a4b620495f5429564308f34c15499f11
danilodinizs <danilodiniz93@yahoo.com>
2025-04-07 10:52:17 -0300

    feat: implements ExerciseRepository

---
+++ src/main/java/dev/danilo/gymanager/repository/ExerciseRepository.java
@@ -0,0 +1,11 @@
+package dev.danilo.gymanager.repository;
+
+import dev.danilo.gymanager.entity.Exercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ExerciseRepository extends JpaRepository<Exercise, UUID> {
+}
