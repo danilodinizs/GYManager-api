8dd1afcb5cbcd2faf048375e4344361e0e95f619
danilodinizs <danilodiniz93@yahoo.com>
2025-04-07 11:41:29 -0300

    feat: implements save exercise endpoint

--- src/main/java/dev/danilo/gymanager/controller/ExerciseController.java
+++ src/main/java/dev/danilo/gymanager/controller/ExerciseController.java
@@ -1,7 +1,13 @@
 package dev.danilo.gymanager.controller;
 
+import dev.danilo.gymanager.dto.ExerciseRequestDTO;
+import dev.danilo.gymanager.dto.ExerciseResponseDTO;
 import dev.danilo.gymanager.mapper.ExerciseMapper;
 import dev.danilo.gymanager.service.ExerciseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +22,10 @@ public class ExerciseController {
     public ExerciseController(ExerciseService service, ExerciseMapper mapper) {
         this.service = service;
         this.mapper = mapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<ExerciseResponseDTO> saveExercise(@RequestBody ExerciseRequestDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.saveExercise(dto));
     }
 }
