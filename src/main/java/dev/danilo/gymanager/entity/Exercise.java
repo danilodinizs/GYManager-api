6dba232c476f7632889cb1873aebdb52edfa7027
danilodinizs <danilodiniz93@yahoo.com>
2025-04-07 11:40:45 -0300

    feat: includes manual getters and setters

--- src/main/java/dev/danilo/gymanager/entity/Exercise.java
+++ src/main/java/dev/danilo/gymanager/entity/Exercise.java
@@ -36,4 +36,76 @@ public class Exercise {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "workout_id")
     private Workout workout;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSets() {
+        return sets;
+    }
+
+    public void setSets(String sets) {
+        this.sets = sets;
+    }
+
+    public String getReps() {
+        return reps;
+    }
+
+    public void setReps(String reps) {
+        this.reps = reps;
+    }
+
+    public String getRestTime() {
+        return restTime;
+    }
+
+    public void setRestTime(String restTime) {
+        this.restTime = restTime;
+    }
+
+    public Integer getExerciseOrder() {
+        return exerciseOrder;
+    }
+
+    public void setExerciseOrder(Integer exerciseOrder) {
+        this.exerciseOrder = exerciseOrder;
+    }
+
+    public String getTechnique() {
+        return technique;
+    }
+
+    public void setTechnique(String technique) {
+        this.technique = technique;
+    }
+
+    public Workout getWorkout() {
+        return workout;
+    }
+
+    public void setWorkout(Workout workout) {
+        this.workout = workout;
+    }
 }
