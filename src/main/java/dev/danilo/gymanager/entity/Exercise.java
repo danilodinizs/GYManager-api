da7e68fc4b46af5ec9c1182130e50dc74bb329f2
danilodinizs <danilodiniz93@yahoo.com>
2025-04-07 10:45:04 -0300

    feat: implements exercise entity

--- src/main/java/dev/danilo/gymanager/entity/Exercise.java
+++ src/main/java/dev/danilo/gymanager/entity/Exercise.java
@@ -5,11 +5,27 @@ import jakarta.persistence.*;
 import java.util.UUID;
 
 @Entity
+@Table(name = "exercise")
 public class Exercise {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+    private String description;
+
+    private String sets;
+    private String reps;
+
+    @Column(name = "rest_time")
+    private String restTime;
+
+    @Column(name = "exercise_order")
+    private Integer exerciseOrder;
+
+    private String technique;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "workout_id")
