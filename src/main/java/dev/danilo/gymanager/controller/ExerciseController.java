25e0e9cf3ef22f39fc4acb2b1fa805b2d7572e3c
danilodinizs <danilodiniz93@yahoo.com>
2025-04-07 10:52:48 -0300

    feat: creates Exercise controller class

---
+++ src/main/java/dev/danilo/gymanager/controller/ExerciseController.java
@@ -0,0 +1,9 @@
+package dev.danilo.gymanager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exercise")
+public class ExerciseController {
+}
