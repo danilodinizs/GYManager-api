04a2936c7eef1c758d38dc7f46fb05a2783c129b
danilodinizs <danilodiniz93@yahoo.com>
2025-04-07 11:13:27 -0300

    feat: implements dependencies injection

--- src/main/java/dev/danilo/gymanager/controller/ExerciseController.java
+++ src/main/java/dev/danilo/gymanager/controller/ExerciseController.java
@@ -1,9 +1,20 @@
 package dev.danilo.gymanager.controller;
 
+import dev.danilo.gymanager.mapper.ExerciseMapper;
+import dev.danilo.gymanager.service.ExerciseService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/exercise")
 public class ExerciseController {
+
+    private final ExerciseService service;
+    private final ExerciseMapper mapper;
+
+
+    public ExerciseController(ExerciseService service, ExerciseMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
 }
--- src/main/java/dev/danilo/gymanager/service/ExerciseService.java
+++ src/main/java/dev/danilo/gymanager/service/ExerciseService.java
@@ -1,7 +1,19 @@
 package dev.danilo.gymanager.service;
 
+import dev.danilo.gymanager.mapper.ExerciseMapper;
+import dev.danilo.gymanager.repository.ExerciseRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExerciseService {
+
+    private final ExerciseRepository repository;
+
+    private final ExerciseMapper mapper;
+
+
+    public ExerciseService(ExerciseRepository repository, ExerciseMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
 }
