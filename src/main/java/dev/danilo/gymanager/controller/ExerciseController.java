b6782ccf2592f1e1e2b17c297aa3d56db1438c74
danilodinizs <danilodiniz93@yahoo.com>
2025-04-08 17:13:53 -0300

    feat: implements get all exercises endpoint

--- src/main/java/dev/danilo/gymanager/controller/ExerciseController.java
+++ src/main/java/dev/danilo/gymanager/controller/ExerciseController.java
@@ -6,10 +6,9 @@ import dev.danilo.gymanager.mapper.ExerciseMapper;
 import dev.danilo.gymanager.service.ExerciseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/exercise")
@@ -27,5 +26,10 @@ public class ExerciseController {
     @PostMapping
     public ResponseEntity<ExerciseResponseDTO> saveExercise(@RequestBody ExerciseRequestDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.saveExercise(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ExerciseResponseDTO>> findAll() {
+        return ResponseEntity.ok().body(service.findAll());
     }
 }
