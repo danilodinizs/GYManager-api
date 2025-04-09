b64661cafecaa135ab22f64a6283a932b4640b96
danilodinizs <danilodiniz93@yahoo.com>
2025-04-09 15:33:16 -0300

    feat: implements register user business rule

--- src/main/java/dev/danilo/gymanager/service/UserService.java
+++ src/main/java/dev/danilo/gymanager/service/UserService.java
@@ -1,7 +1,27 @@
 package dev.danilo.gymanager.service;
 
+import dev.danilo.gymanager.dto.UserRequestDTO;
+import dev.danilo.gymanager.dto.UserResponseDTO;
+import dev.danilo.gymanager.entity.User;
+import dev.danilo.gymanager.mapper.UserMapper;
+import dev.danilo.gymanager.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserRepository repository;
+    private final UserMapper mapper;
+
+    public UserService(UserRepository repository, UserMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+
+    public UserResponseDTO save(UserRequestDTO dto) {
+        return mapper.toDto(repository.save(mapper.toEntity(dto)));
+    }
+
 }
