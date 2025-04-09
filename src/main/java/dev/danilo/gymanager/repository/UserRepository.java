fef88c4eb9b4b5223d597974e1397662601aab07
danilodinizs <danilodiniz93@yahoo.com>
2025-04-09 13:55:18 -0300

    feat: creates and implements UserRepository

---
+++ src/main/java/dev/danilo/gymanager/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dev.danilo.gymanager.repository;
+
+import dev.danilo.gymanager.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
