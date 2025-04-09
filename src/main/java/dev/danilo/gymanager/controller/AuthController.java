8e81d086eceb44cb7218f446c7b7f1c14472e524
danilodinizs <danilodiniz93@yahoo.com>
2025-04-09 15:33:01 -0300

    feat: implements register user endpoint

--- src/main/java/dev/danilo/gymanager/controller/AuthController.java
+++ src/main/java/dev/danilo/gymanager/controller/AuthController.java
@@ -1,6 +1,11 @@
 package dev.danilo.gymanager.controller;
 
+import dev.danilo.gymanager.dto.UserRequestDTO;
+import dev.danilo.gymanager.dto.UserResponseDTO;
 import dev.danilo.gymanager.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,10 @@ public class AuthController {
 
     public AuthController(UserService service) {
         this.service = service;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> register(@RequestBody UserRequestDTO dto) {
+        return ResponseEntity.ok(service.save(dto));
     }
 }
