2bab72b149578931ea858caee3b3943c89b60b3f
danilodinizs <danilodiniz93@yahoo.com>
2025-04-09 15:32:01 -0300

    feat: includes @Builder annotation

--- src/main/java/dev/danilo/gymanager/dto/UserResponseDTO.java
+++ src/main/java/dev/danilo/gymanager/dto/UserResponseDTO.java
@@ -1,7 +1,10 @@
 package dev.danilo.gymanager.dto;
 
+import lombok.Builder;
+
 import java.util.UUID;
 
+@Builder
 public record UserResponseDTO(UUID id,
                               String name,
                               String email) {
