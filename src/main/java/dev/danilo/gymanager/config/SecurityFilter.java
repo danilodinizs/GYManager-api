86dd5bdddaa26efb8de231d3db9d7fc06b823e0c
danilodinizs <danilodiniz93@yahoo.com>
2025-04-15 17:49:13 -0300

    feat: implements security filter class

---
+++ src/main/java/dev/danilo/gymanager/config/SecurityFilter.java
@@ -0,0 +1,45 @@
+package dev.danilo.gymanager.config;
+
+import dev.danilo.gymanager.repository.UserRepository;
+import dev.danilo.gymanager.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService service;
+    private final UserRepository repository;
+
+    public SecurityFilter(TokenService service, UserRepository repository) {
+        this.service = service;
+        this.repository = repository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if(token != null) {
+            var subject = service.validateToken(token);
+            UserDetails user = repository.findByEmail(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer", "");
+    }
+}
