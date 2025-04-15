29ca9ecf5fa01a6836afc031fac1b700211b3333
Danilo Diniz <danilodiniz93@yahoo.com>
2025-04-14 23:15:14 -0300

    feat: implements extra security configurations

--- src/main/java/dev/danilo/gymanager/config/SecurityConfig.java
+++ src/main/java/dev/danilo/gymanager/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package dev.danilo.gymanager.config;
 
+import jdk.jfr.Experimental;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +28,15 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/v1/spreadsheet").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder()  {
+        return new BCryptPasswordEncoder();
     }
 }
