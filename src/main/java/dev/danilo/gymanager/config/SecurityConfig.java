c6f1145e293ed673dc8484e1e9e4acb3a2512a31
danilodinizs <danilodiniz93@yahoo.com>
2025-04-15 17:48:04 -0300

    feat: finishes security config class

--- src/main/java/dev/danilo/gymanager/config/SecurityConfig.java
+++ src/main/java/dev/danilo/gymanager/config/SecurityConfig.java
@@ -14,10 +14,17 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final SecurityFilter securityFilter;
+
+    public SecurityConfig(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -30,7 +37,9 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.DELETE, "/auth/delete/{email}").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET, "/auth").hasRole("ADMIN")
                         // .requestMatchers(HttpMethod.GET, "/v1/spreadsheet").hasRole("ADMIN")
-                        .anyRequest().authenticated())
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
