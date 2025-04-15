a3c2c9a7b41344897080b91e84dc1089f9ede5cb
Danilo Diniz <danilodiniz93@yahoo.com>
2025-04-14 21:28:25 -0300

    feat: implements login endpoint

--- src/main/java/dev/danilo/gymanager/controller/AuthController.java
+++ src/main/java/dev/danilo/gymanager/controller/AuthController.java
@@ -3,7 +3,10 @@ package dev.danilo.gymanager.controller;
 import dev.danilo.gymanager.dto.UserRequestDTO;
 import dev.danilo.gymanager.dto.UserResponseDTO;
 import dev.danilo.gymanager.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final AuthenticationManager authenticationManager;
     private final UserService service;
 
-    public AuthController(UserService service) {
+    public AuthController(AuthenticationManager authenticationManager, UserService service) {
+        this.authenticationManager = authenticationManager;
         this.service = service;
     }
 
     @PostMapping("/register")
     public ResponseEntity<UserResponseDTO> register(@RequestBody UserRequestDTO dto) {
         return ResponseEntity.ok(service.save(dto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid UserRequestDTO dto) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
     }
 }
