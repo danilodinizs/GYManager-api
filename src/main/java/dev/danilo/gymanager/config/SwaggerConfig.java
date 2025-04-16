23a15797b85897e55c31e32ea3f8fa2b544c12a4
danilodinizs <danilodiniz93@yahoo.com>
2025-04-16 18:36:42 -0300

    feat: adds swaggerconfig class

---
+++ src/main/java/dev/danilo/gymanager/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package dev.danilo.gymanager.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("GYManager-api")
+                        .version("1.0")
+                        .description("API with JWT Authentication"))
+                // Define o esquema de segurança (JWT)
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
+                // Aplica o esquema globalmente (opcional)
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+}
